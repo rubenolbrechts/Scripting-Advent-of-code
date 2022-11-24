2ff15a0d32786dc25145ac36c41735183c1666f4
Ruben Olbrechts <ruben.olbrechts@student.howest.be>
2022-11-24 09:12:51 +0100

    Upload first file

---
+++ lesson01/example-code-1-1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import sys
+print("Hello, I'm Fedora.")
+print("You're using Python " + sys.version + ".")
+# Ask for input and print
+name = input("What is your name? ")
+print("Hello, " + name + "!")
