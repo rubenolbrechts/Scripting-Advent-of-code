4f5cb0b458a8dcff31683145cc859d51135476ea
Ruben Olbrechts <118811332+rubenolbrechts@users.noreply.github.com>
2023-02-11 15:11:27 +0100

    Delete lesson01 directory

--- lesson01/Exercise2.2.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-seq='MNKMDLVADVAEKTDLSKAKATEVIDAVFA'
-print(len(seq))
-print(seq[:25:-1])
-
--- lesson01/exercise2.3.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-charged=["Arg", "Lys"]
-negative=["Asp", "Glu"]
-charged.extend(negative)
-print(charged)
-
-# OR charged.extend(["Asp","Glu"])
